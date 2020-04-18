d3af456ac7e43f3b5e831ac4f0d488ffe13f39ab
Razvan Florescu <razvan@Dianas-MacBook-Air.local>
2020-04-18 15:33:41 +0300

    fix add podcast route

--- src/router.js
+++ src/router.js
@@ -25,7 +25,7 @@ export default new Router({
             component: addPodcast
         }, {
             path: '*',
-            component: PodcastsList,
+            redirect: '/'
         }
     ]
 })
