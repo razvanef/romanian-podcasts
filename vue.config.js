9c320de5aa42ccf7b9b496c0a60e7a436f60e43e
Razvan Florescu <razvan@Dianas-MacBook-Air.local>
2020-04-21 20:10:07 +0300

    change site title

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  chainWebpack: config => {
+    config
+    .plugin('html')
+    .tap(args => {
+      args[0].title = 'Podcasturi Romanesti'
+      return args
+    })
+  }
 };
