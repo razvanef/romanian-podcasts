9818e48e77fd33e2c3af022b92b2fe55c531467c
Razvan Florescu <razvan@Dianas-MacBook-Air.local>
2020-04-25 16:56:55 +0300

    add ga

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import router from "./router";
+import VueAnalytics from 'vue-analytics';
 
 Vue.config.productionTip = false;
+
+// Configuration VueAnalytics
+Vue.use(VueAnalytics, {
+  id: process.env.VUE_APP_ANALYTICS_KEY
+});
 
 new Vue({
   vuetify,
